8c0c3986b2f2dbc0fb9cd2aa0fee65fb71bf100c
roylines <roy@roylines.co.uk>
2014-01-21 13:29:25 +0000

    added first request test for /

--- test/routes.js
+++ test/routes.js
@@ -1,7 +1,20 @@
-describe('Google searching', function() {
-  before(function() {
-  });
+var assert = require('assert'),
+  routes = require('../lib/routes'),
+  request = require('request');
 
-  it('should', function() {
+var get = function(url, done) {
+  return request('http://localhost:8000' + url, done);
+};
+
+var assert200 = function(response) {
+  assert.equal(response.statusCode, 200);
+};
+
+describe('routes', function() {
+  it('calling / should return 200', function(done) {
+    get('/', function(e, response, body) {
+      assert200(response); 
+      return done();
+    });
   });
 });
