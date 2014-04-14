c061484c446b25e6aafac406016a080b30c5cb86
roylines <roy@roylines.co.uk>
2014-04-14 19:15:18 +0100

    started upgrade to express 4

--- gulp/deploy.js
+++ gulp/deploy.js
@@ -1,8 +1,13 @@
 var deploy = {},
+  exec = require('gulp-exec'),
   gulp = require('gulp');
 
 var version = new Date().getTime();
 var packaged = 'packaged/' + version;
+var remote = '/home/nodeuser/projects';
+var remoteCurrent = remote + '/current';
+var remotePackaged = remote + '/' + version;
+var ip = process.env.ZZ_REMOTE;
 
 deploy.package = function(dir) {
   return function() {
@@ -20,12 +25,34 @@ deploy.packageRoot = function() {
     .pipe(gulp.dest(packaged));
 };
 
+deploy.scp = function() {
+  var ssh = function(user, commands) {
+    var cmd = 'ssh ' + user + '@' + ip + ' \'' + commands + '\'';
+    console.log(cmd);
+    return cmd;
+  }
+
+  var scp = function(user, source, target, done) {
+    var cmd = 'scp -Cr ' + source + ' ' + user + '@' + ip + ':' + target;
+    console.log(cmd);
+    return cmd;
+  }
+
+  return gulp.src('')
+    .pipe(exec(scp('nodeuser', packaged, remotePackaged)))
+    .pipe(exec(ssh('nodeuser', 'set -e; cd ' + remotePackaged + ';npm install --production')))
+    .pipe(exec(ssh('nodeuser', 'set -e; ln -s ' + remotePackaged + ' ' + remoteCurrent)))
+    .pipe(exec(ssh('nodeuser', 'set -e; cd ' + remote + ';naught start current/app.js;naught deploy')));
+};
+
 gulp.task('deploy-package-statics', deploy.package('static'));
 gulp.task('deploy-package-views', deploy.package('views'));
 gulp.task('deploy-package-models', deploy.package('models'));
 gulp.task('deploy-package-lib', deploy.package('lib'));
 gulp.task('deploy-package-root', deploy.packageRoot);
 gulp.task('deploy-package', ['deploy-package-root', 'deploy-package-statics', 'deploy-package-views', 'deploy-package-models', 'deploy-package-lib']);
-gulp.task('deploy-all', ['deploy-package']);
+gulp.task('deploy-scp', ['deploy-package'], deploy.scp);
+
+gulp.task('deploy-all', ['deploy-scp']);
 
 module.exports = deploy;
